579b89d18627e2dd756819e988978be039cc624a
Jesse Billard <jessebillard@mac.com>
2020-10-17 23:50:09 -0400

    adds auth without implementing, new star svg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -9,10 +10,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from your special bb lover</h1>
         <br/>
-        <p>Just wanted to say you're doing good work hun :)</p>
+        <p>Just wanted to say you're doing good work hun :)</p>        
       </header>
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
 
 export default App;
+// export default withAuthenticator(App);
