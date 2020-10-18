968c17a4aacb2bc52886045dcbf438ef74ed7be3
Jesse Billard <jessebillard@mac.com>
2020-10-18 00:40:36 -0400

    extra extra exclamation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from your special bb lover!</h1>
+        <h1>Hello from your special bb lover!!</h1>
         <br/>
         <p>Just wanted to say you're doing good work hun :)</p>        
       </header>
